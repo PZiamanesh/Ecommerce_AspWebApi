79c4e14daee39a969feb06e2ea4f13d00ac184a6
PZiamanesh <po.ziamanesh@gmail.com>
2024-03-01 00:16:05 +0330

    layers realtions added

---
+++ src/WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Common;
+
+namespace WebAPI.Controllers
+{
+    public class ProductController : BaseApiController
+    {
+    }
+}
